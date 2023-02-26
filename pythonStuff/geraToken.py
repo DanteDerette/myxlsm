ec03b687124afbcf4e31eefb9fc370e39281f88c
DanteDerette <sdanted@hotmail.com>
2023-02-26 13:59:28 -0300

    asdasd

---
+++ pythonStuff/geraToken.py
@@ -0,0 +1,13 @@
+
+import requests
+
+# curl -k -X POST -H "Authorization: Basic MDI3NDIzNjEwMDAxMzk6YjcxN2E4NzYtNWYwZC00NzMxLTlmNjgtOGMzZWE0ZjU2Yzkw" -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials" "https://jpautomacao-getcard02.getcard.uniplusweb.com/oauth/token"
+
+headers = "Basic MDI3NDIzNjEwMDAxMzk6YjcxN2E4NzYtNWYwZC00NzMxLTlmNjgtOGMzZWE0ZjU2Yzkw" 
+ContentType = "application/x-www-form-urlencoded"
+url = "https://jpautomacao-getcard02.getcard.uniplusweb.com/oauth/token"
+data = "grant_type=client_credentials"
+
+
+response = requests.post(url, headers={"Authorization":headers, "Content-Type": "application/x-www-form-urlencoded"}, data=data)
+print(response.text)
