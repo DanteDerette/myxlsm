99a3492e987857263537918c0714a98a41439f49
DanteDerette <sdanted@hotmail.com>
2023-02-26 14:44:33 -0300

    ddd

--- pythonStuff/geraToken.py
+++ pythonStuff/geraToken.py
@@ -1,13 +1,16 @@
 
 import requests
+import json
 
 # curl -k -X POST -H "Authorization: Basic MDI3NDIzNjEwMDAxMzk6YjcxN2E4NzYtNWYwZC00NzMxLTlmNjgtOGMzZWE0ZjU2Yzkw" -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials" "https://jpautomacao-getcard02.getcard.uniplusweb.com/oauth/token"
 
-headers = "Basic MDI3NDIzNjEwMDAxMzk6YjcxN2E4NzYtNWYwZC00NzMxLTlmNjgtOGMzZWE0ZjU2Yzkw" 
-ContentType = "application/x-www-form-urlencoded"
-url = "https://jpautomacao-getcard02.getcard.uniplusweb.com/oauth/token"
-data = "grant_type=client_credentials"
+def myNewToken():
+    headers = "Basic MDI3NDIzNjEwMDAxMzk6YjcxN2E4NzYtNWYwZC00NzMxLTlmNjgtOGMzZWE0ZjU2Yzkw" 
+    ContentType = "application/x-www-form-urlencoded"
+    url = "https://jpautomacao-getcard02.getcard.uniplusweb.com/oauth/token"
+    data = "grant_type=client_credentials"
 
+    response = requests.post(url, headers={"Authorization":headers, "Content-Type": "application/x-www-form-urlencoded"}, data=data)
 
-response = requests.post(url, headers={"Authorization":headers, "Content-Type": "application/x-www-form-urlencoded"}, data=data)
-print(response.text)
+    access_token = str(json.loads(response.text)["access_token"])
+    return access_token
--- pythonStuff/lerProdutoPeloCod.py
+++ pythonStuff/lerProdutoPeloCod.py
@@ -3,24 +3,17 @@ import decimal
 import requests
 import math
 import json
+from geraToken import myNewToken
 
 url = "https://jpautomacao-getcard02.getcard.uniplusweb.com/public-api/v1/produtos"
 
 
 headers = {
     "Content-Type": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsidW5pcGx1c3dlYiJdLCJzY29wZSI6WyJwZHYiLCJqb2JzLXBkdiIsIm1vYmlsZSIsInNob3AiLCJwdWJsaWMtYXBpIl0sImV4cCI6MTY3NzQyNDU5NywianRpIjoiNTk4YTlkYWMtMDFmNS00ZmIyLTkzYTktYTc2YTFmM2Y3NmI4IiwidGVuYW50IjpudWxsLCJjbGllbnRfaWQiOiIwMjc0MjM2MTAwMDEzOSJ9.NijXZ5BVJHVPun-mIHmXy3da7pDG88MFOB9VnJOGG-Y"
+    "Authorization": "Bearer " + myNewToken() 
 }
 
 payload = {"produto": {"nome": "PFG132"}}
 response = requests.request("GET", url, json=payload, headers=headers)
 
 print(json.loads(response.text)[0]['codigo'])
-
-
-# {“{“produto”: {“codigo”: “999000”, “nome”:”PRODUTO TESTE”, “preco”: 10.23}}
-
- 
-
-
-
